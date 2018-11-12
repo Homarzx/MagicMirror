4fd5b495a8cc98fcfd95ec62798ca8d24936b57b
JOel <joelerll@gmail.com>
2018-11-11 23:27:21 -0500

    camera cambios

--- camera.py
+++ camera.py
@@ -1,9 +1,18 @@
 import picamera
 import time
 
+def recordingvideo():
+    date_recording = '/home/pi/Videos/video_date'+time.strftime("%c")+'.h264'
+    camera = picamera.Picamera()
+    camera.resolution(640, 480)
+    camera.start_recording(date_recording)
+    camera.wait_recording(60)
+    camera.stop_recording()
+
 with picamera.PiCamera() as camera:
 	camera.resolution = 'HD'
 	camera.start_preview()
 	time.sleep(10)
 	camera.capture('/home/pi/Pictures/image.jpg')
 	camera.stop_preview()
+
